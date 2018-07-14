669aa232fbcd0ef9f54c5a0ee9b99e8eabc9beef
Sandy Edwards <sedwardscode@gmail.com>
2018-07-14 12:59:08 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+let currentUser = "Sandy";
+let welcomeMessage = "Welcome to Flatbook, " + currentUser + "!";
+let excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + currentUser.toUpperCase() + "!";
+let shortGreeting = "Welcome, " + currentUser.charAt(0) + "!"
